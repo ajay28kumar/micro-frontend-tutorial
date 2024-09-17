04e326a8fe35b3642d7128834eab51ba276be002
Ajay Kumar <ajaykumar@Ajays-MacBook-Pro.local>
2024-09-17 11:58:34 +0530

    updated env

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/marketing-entry.js`,
+        marketing: `marketing@${domain}/marketing/latest/marketing-entry.js`,
       },
       shared: packageJson.dependencies
     })
