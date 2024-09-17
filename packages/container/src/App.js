d1782dac6d2f388b32f4231e34466ed416c65d1f
Ajay Kumar <ajaykumar@Ajays-MacBook-Pro.local>
2024-09-17 19:07:17 +0530

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => {
   return (
-    <div>
-      <nav>
-        <div>
-          TODO: Header Area Design
-        </div>
-      </nav>
-      <hr/>
-      <MarketingApp/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header/>
+        <MarketingApp/>
+      </div>
+    </BrowserRouter>
   )
 };
 
